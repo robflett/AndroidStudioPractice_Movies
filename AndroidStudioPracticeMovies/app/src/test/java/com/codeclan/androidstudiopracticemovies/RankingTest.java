3d428d55cab4fc8300d4c2b09d947c195294a9e4
Rob <robflettcc@gmail.com>
2017-03-13 18:45:46 +0000

    Added another movie to the rankings

--- AndroidStudioPracticeMovies/app/src/test/java/com/codeclan/androidstudiopracticemovies/RankingTest.java
+++ AndroidStudioPracticeMovies/app/src/test/java/com/codeclan/androidstudiopracticemovies/RankingTest.java
@@ -14,18 +14,21 @@ public class RankingTest {
 
     Ranking movies;
     Movie movie;
+    Movie movie2;
 
     @Before
     public void before() {
         movies = new Ranking();
         movie = new Movie("Face/Off", "Action", 1);
+        movie2 = new Movie("Jaws", "Thriller", 2);
         movies.addMovie(movie);
+        movies.addMovie(movie2);
     }
 
     @Test
     public void testAddMovie(){
 
-        assertEquals(1, movies.getLength() );
+        assertEquals(2, movies.getLength() );
     }
 
 }
