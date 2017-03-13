a2ba06df1a8c51376908bdbae5af09a217bf7f9d
Rob <robflettcc@gmail.com>
2017-03-13 18:43:01 +0000

    Ranking class set up and first test passed

--- AndroidStudioPracticeMovies/app/src/main/java/com/codeclan/androidstudiopracticemovies/Ranking.java
+++ AndroidStudioPracticeMovies/app/src/main/java/com/codeclan/androidstudiopracticemovies/Ranking.java
@@ -1,8 +1,29 @@
 package com.codeclan.androidstudiopracticemovies;
 
+import java.util.ArrayList;
+
 /**
  * Created by user on 13/03/2017.
  */
 
-public class Ranking {
+    public class Ranking {
+
+        private ArrayList<Movie> movies;
+
+
+    public Ranking(){
+        this.movies = new ArrayList<Movie>();
+    }
+
+    public ArrayList<Movie> getMovies() {
+        return movies;
+    }
+
+    public void addMovie(Movie movie){
+        movies.add(movie);
+    }
+
+    public int getLength() {
+        return movies.size();
+    }
 }
--- AndroidStudioPracticeMovies/app/src/test/java/com/codeclan/androidstudiopracticemovies/RankingTest.java
+++ AndroidStudioPracticeMovies/app/src/test/java/com/codeclan/androidstudiopracticemovies/RankingTest.java
@@ -1,11 +1,31 @@
 package com.codeclan.androidstudiopracticemovies;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
 /**
  * Created by user on 13/03/2017.
  */
 
 public class RankingTest {
 
+    Ranking movies;
+    Movie movie;
 
+    @Before
+    public void before() {
+        movies = new Ranking();
+        movie = new Movie("Face/Off", "Action", 1);
+        movies.addMovie(movie);
+    }
+
+    @Test
+    public void testAddMovie(){
+
+        assertEquals(1, movies.getLength() );
+    }
 
 }
